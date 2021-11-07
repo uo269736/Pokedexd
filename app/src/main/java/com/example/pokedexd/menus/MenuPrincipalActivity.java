3d58e75fc2768156ca6927c06272675bf050791b
Sergio <Sergio.vega.20@um.edu.mt>
2021-11-07 23:39:33 +0100

    Se introduce el inicio de sesión utilizando firebase

--- app/src/main/java/com/example/pokedexd/menus/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/pokedexd/menus/MenuPrincipalActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.pokedexd.R;
+import com.example.pokedexd.autenticacion.RegistroActivity;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
     private Button btnMenuBuscador;
     private Button btnMenuEquipo;
     private Button btnMenuLiga;
+    private Button btnIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +25,21 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         btnMenuBuscador = (Button) findViewById(R.id.MenuBtnBuscador);
         btnMenuEquipo = (Button) findViewById(R.id.MenuBtnEquipo);
         btnMenuLiga = (Button) findViewById(R.id.MenuBtnLiga);
+        btnIniciarSesion = (Button) findViewById(R.id.menuBtnInicioSesion);
 
         //Pasar a las respectivas pantallas al clickar el boton.
-        btnMenuBuscador.setOnClickListener(new View.OnClickListener() {
+        btnMenuBuscador.setOnClickListener(new View.OnClickListener() { //Pasar al buscador cuadno se clicka
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MenuPrincipalActivity.this, MenuBuscadorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPrincipalActivity.this, RegistroActivity.class);
                 startActivity(intent);
             }
         });
